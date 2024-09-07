1065bd42674f34f39bf76c6d5498f25ea9ddbbee
Mokalbari <150693283+Mokalbari@users.noreply.github.com>
2024-09-07 15:10:27 +0200

    add title component to destination

---
+++ app/destination/page.tsx
@@ -0,0 +1,11 @@
+// type Props = {}
+
+import Title from "../ui/destination/title"
+
+export default function Page() {
+  return (
+    <main>
+      <Title />
+    </main>
+  )
+}
---
+++ app/ui/destination/title.tsx
@@ -0,0 +1,11 @@
+import { barlowCondensed } from "../fonts"
+
+export default function Title() {
+  return (
+    <h1
+      className={`${barlowCondensed.className} uppercase tracking-wider sm:text-[1.25rem] lg:text-md`}
+    >
+      <span className="font-bold text-white/50">01</span> Pick you destination
+    </h1>
+  )
+}
