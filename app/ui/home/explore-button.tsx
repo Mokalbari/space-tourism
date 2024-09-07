b5161617d7e1b91d93922ed3a4c887171baa8eb6
Mokalbari <150693283+Mokalbari@users.noreply.github.com>
2024-09-07 11:03:14 +0200

    add explore button component

--- app/ui/home/explore-button.tsx
+++ app/ui/home/explore-button.tsx
@@ -1,10 +1,17 @@
 // type Props = {}
 import { bellefair } from "../fonts"
+import clsx from "clsx"
 
 export default function ExploreButton() {
   return (
     <button
-      className={`${bellefair.className} relative aspect-square cursor-pointer rounded-full bg-white p-12 uppercase text-black hover:after:absolute hover:after:bg-red-600`}
+      className={clsx(
+        bellefair.className,
+        "relative z-10 aspect-square cursor-pointer rounded-full bg-white px-8 text-sm uppercase text-black", // Classes principales
+        "px-16 sm:text-lg", // Responsive
+        "after:absolute after:left-0 after:top-0 after:-z-10 after:h-full after:w-full after:rounded-full", // Styles pour ::after
+        "after:bg-white/10 after:transition after:ease-in-out hover:after:scale-150 hover:after:transition-transform", // Styles au hover
+      )}
     >
       Explore
     </button>
